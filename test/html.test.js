fde47ee2d6a9b224921e8aca46d7fa4dbabd7b2c
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-15 16:59:42 +0700

    Configure to test local HTML file via path & fs

--- test/html.test.js
+++ test/html.test.js
@@ -1,19 +1,19 @@
 const path = require('path')
-const HTML = path.join(__dirname, '../src/index.html')
+const fs = require('fs')
+
+const HTML_PATH = path.join(__dirname, '../src/index.html')
+const HTML_STRING = fs.readFileSync(HTML_PATH, 'utf8')
 
 const timeout = 5000
 
-describe.skip(
+describe(
   'GET / (Home Page)',
   () => {
     let page
 
     beforeAll(async () => {
-      console.log(HTML)
-
       page = await global.__BROWSER__.newPage()
-      await page.setContent(HTML)
-      await page.goto(HTML)
+      await page.setContent(HTML_STRING)
     }, timeout)
 
     it('should load without error', async () => {
